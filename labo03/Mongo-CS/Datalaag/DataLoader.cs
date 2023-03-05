ffca19355950ce29d602fd0f90dd8fd6c42c3e16
Kenzo Staelens <kenzo.staelens@student.odisee.be>
2023-03-05 12:24:10 +0100

    tests mongo

--- labo03/Mongo-CS/Datalaag/DataLoader.cs
+++ labo03/Mongo-CS/Datalaag/DataLoader.cs
@@ -12,11 +12,19 @@ namespace Datalaag {
         private MongoClient _client;
         private IMongoDatabase _db;
         public DataLoader() {
-            BsonClassMap.RegisterClassMap<Member>();
+            BsonClassMap.RegisterClassMap<Coach>();
+            BsonClassMap.RegisterClassMap<Swimmer>();
             BsonClassMap.RegisterClassMap<Workout>();
             BsonClassMap.RegisterClassMap<SwimmingPool>();
             _client = new MongoClient("mongodb://localhost");
             _db = _client.GetDatabase("test");
+            IMongoCollection<Swimmer> swimmers = _db.GetCollection<Swimmer>("swimmers");
+            var w = new Workout() { Id=1,Duration=5,Coach=null, Schedule=DateTime.Now, Swimmingpool=null, Type=WorkoutType.ENDURANCE};
+            var s = new Swimmer() { FirstName="abc", LastName="def", DateOfBirth = DateTime.Now, Gender='M', FinalPoints=0,Workouts=new List<Workout>() { w }, Id=1};
+            swimmers.InsertOne(s);
+            var t = swimmers.Find(x=>true).Single();
+            Console.WriteLine($"{t.FirstName} {t.LastName} {t.DateOfBirth} {t.Gender} {t.FinalPoints} {t.Id} \n");
+            Console.WriteLine($"{t.Workouts[0].Id} {t.Workouts[0].ToString()}");
         }
 
 
